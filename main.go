1463a345605131ec79342287ae85ec8b8dfd79f4
gpeers <gpeers@chef.io>
2018-09-12 11:59:03 -0700

    initial datacenter vms listing

---
+++ main.go
@@ -0,0 +1,151 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"net/url"
+	"os"
+	"strings"
+
+	"github.com/vmware/govmomi"
+	"github.com/vmware/govmomi/vim25"
+	"github.com/vmware/govmomi/vim25/soap"
+	"log"
+	"github.com/vmware/govmomi/view"
+	"github.com/vmware/govmomi/vim25/mo"
+	"text/tabwriter"
+)
+
+// getEnvString returns string from environment variable.
+func getEnvString(v string, def string) string {
+	r := os.Getenv(v)
+	if r == "" {
+		return def
+	}
+
+	return r
+}
+
+// getEnvBool returns boolean from environment variable.
+func getEnvBool(v string, def bool) bool {
+	r := os.Getenv(v)
+	if r == "" {
+		return def
+	}
+
+	switch strings.ToLower(r[0:1]) {
+	case "t", "y", "1":
+		return true
+	}
+
+	return false
+}
+
+const (
+	envURL      = "GOVMOMI_URL"
+	envUserName = "GOVMOMI_USERNAME"
+	envPassword = "GOVMOMI_PASSWORD"
+	envInsecure = "GOVMOMI_INSECURE"
+)
+
+var urlDescription = fmt.Sprintf("ESX or vCenter URL [%s]", envURL)
+var urlFlag = flag.String("url", getEnvString(envURL, "https://username:password@host"+vim25.Path), urlDescription)
+
+var insecureDescription = fmt.Sprintf("Don't verify the server's certificate chain [%s]", envInsecure)
+var insecureFlag = flag.Bool("insecure", getEnvBool(envInsecure, false), insecureDescription)
+
+func processOverride(u *url.URL) {
+	envUsername := os.Getenv(envUserName)
+	envPassword := os.Getenv(envPassword)
+
+	// Override username if provided
+	if envUsername != "" {
+		var password string
+		var ok bool
+
+		if u.User != nil {
+			password, ok = u.User.Password()
+		}
+
+		if ok {
+			u.User = url.UserPassword(envUsername, password)
+		} else {
+			u.User = url.User(envUsername)
+		}
+	}
+
+	// Override password if provided
+	if envPassword != "" {
+		var username string
+
+		if u.User != nil {
+			username = u.User.Username()
+		}
+
+		u.User = url.UserPassword(username, envPassword)
+	}
+}
+
+// NewClient creates a govmomi.Client for use in the examples
+func NewClient(ctx context.Context) (*govmomi.Client, error) {
+	flag.Parse()
+
+	// Parse URL from string
+	u, err := soap.ParseURL(*urlFlag)
+	if err != nil {
+		return nil, err
+	}
+
+	// Override username and/or password as required
+	processOverride(u)
+
+	// Connect and log in to ESX or vCenter
+	return govmomi.NewClient(ctx, u, *insecureFlag)
+}
+
+func main() {
+	ctx := context.Background()
+
+	c, err := NewClient(ctx)
+	if err != nil {
+		log.Fatal(err) MM
+	}
+
+	defer c.Logout(ctx)
+
+	info := c.ServiceContent.About
+	fmt.Printf("\nConnected to %s, version %s - %s\n\n", info.Name, info.Version, info.InstanceUuid)
+
+	// Create view of VirtualMachine objects
+	m := view.NewManager(c.Client)
+
+	v, err := m.CreateContainerView(ctx, c.ServiceContent.RootFolder, []string{"VirtualMachine"}, true)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer v.Destroy(ctx)
+
+	// Retrieve summary property for all machines
+	// Reference: http://pubs.vmware.com/vsphere-60/topic/com.vmware.wssdk.apiref.doc/vim.VirtualMachine.html
+	var vms []mo.VirtualMachine
+	err = v.Retrieve(ctx, []string{"VirtualMachine"}, []string{ "summary" }, &vms)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Print summary per vm (see also: govc/vm/info.go)
+	fmt.Printf("Datacenter VMs\n\n")
+	w := new(tabwriter.Writer)
+
+	// Format in tab-separated columns with a tab stop of 5.
+	w.Init(os.Stdout, 0, 8, 0, '\t', 0)
+
+	for _, vm := range vms {
+		fmt.Fprintf(w, "%s\t%s\t%s\n", vm.Summary.Config.Name, vm.Summary.Config.GuestFullName, vm.Summary.Config.InstanceUuid)
+	}
+
+	w.Flush()
+
+}
